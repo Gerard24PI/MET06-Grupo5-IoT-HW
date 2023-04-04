7f80771d1253ae5847f9024b396a8ecd1487ae6a
Gerard PI <gerard.perez@students.salle.url.edu>
2023-04-04 12:30:46 +0200

    Adding features to LED - Pending the logic brightness

--- LED.cpp
+++ LED.cpp
@@ -7,15 +7,41 @@ LED::LED() {
 LED::LED(int pin) {
   this->pin = pin;
   pinMode(pin, OUTPUT);
+  this->led_period_ms = LED_PERIOD_MS;
   turnOff();
+  this->led_state = 0;
+  led_timer = Timer();
 }
 
 void LED::turnOn() {
   digitalWrite(pin, HIGH);
+  this->brightness_pct = 100;
+  this->led_period_ms = getMsFromBrightnessPct();
 }
 
 void LED::turnOff() {
   digitalWrite(pin, LOW);
+  this->brightness_pct = 0;
+  this->led_period_ms = getMsFromBrightnessPct();
+}
+
+/**
+ * 0 <= brightness <= 100
+**/
+void LED::setBrightness(int brightness) {
+  this->brightness_pct = brightness;
+}
+
+int LED::getBrightness() {
+  return brightness_pct;
+}
+
+void LED::setLedPeriod(int period) {
+  this->led_period_ms = period;
+}
+
+void LED::updateLED_routine() {
+
 }
 
 void LED::test() {
@@ -23,4 +49,9 @@ void LED::test() {
   delay(1000);
   turnOff();
   delay(1000);
+}
+
+
+int LED::getMsFromBrightnessPct() {
+  return trunc((brightness_pct/100)*led_period_ms);
 }
--- LED.h
+++ LED.h
@@ -2,6 +2,9 @@
 #define LED_H
 
 #include <Arduino.h>
+#include "Timer.h"
+
+#define LED_PERIOD_MS 20
 
 class LED {
 public:
@@ -10,9 +13,20 @@ public:
   void turnOn();
   void turnOff();
   void test();
+  
+  void setBrightness(int brightness);
+  int getBrightness();
+  void setLedPeriod(int period);
+  void updateLED_routine();
 
 private:
   int pin;
+  int led_state;
+  int led_period_ms;
+  int led_high_ms;
+  int brightness_pct;
+  Timer led_timer;
+  int getMsFromBrightnessPct();
 };
 
 #endif
