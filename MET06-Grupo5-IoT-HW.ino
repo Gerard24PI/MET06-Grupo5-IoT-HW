f3063c21160a3f65f7f30cfc186d574c9b0791c1
Gerard PI <gerard.perez@students.salle.url.edu>
2023-04-04 20:55:55 +0200

    Fixing menu bugs and 3 programs flow

--- MET06-Grupo5-IoT-HW.ino
+++ MET06-Grupo5-IoT-HW.ino
@@ -5,73 +5,96 @@
 #include "LED.h"
 
 // MENU 
+#define MENU_SCAIDAS  1
+#define MENU_GRIFO    2
+#define MENU_SLIGHT   3
+#define MENU_EXIT     4
 
-void setup() {
-  // Setup code. runs once
-  setupSensorCaidas();
+int option;
 
-  Serial.begin(SERIAL_CONNECTION_SPEED);
-  
-  Serial.println("");
+void printConnection() {
   Serial.println(String(millis()/1000) + " --> [INFO] "+ String(BOARD_NAME) + " connected @" + String(SERIAL_CONNECTION_SPEED) + "baud");
-
-  Serial.println("========================================");
-  Serial.println("🚀🚀     Welcome to MET_06 project   🚀🚀");
-  Serial.println("           SELECT AN OPTION             ");
-  Serial.println("========================================");
-  Serial.println("1. Sensor Caidas IoT");
-  Serial.println("2. Grifo IoT");
-  Serial.println("3. Smart Light IoT");
-  Serial.println("4. Salir");
 }
 
-
-
-void loop() {
-  // handle Menu
-  if (Serial.available()) {
-    int choice = Serial.read() - '0'; // convert char to int
-    Serial.println("");
-
-    switch (choice) {
-      case 1:
-        Serial.println("Executing Sensor Caidas...");
-         while (Serial.available() == 0 || Serial.read() != '4') {
-          sensorCaidas();
-        }
-        break;
-
-      case 2:
-        while (Serial.available() == 0 || Serial.read() != '4') {
-          Serial.println("Executing Grifo IoT...");
-        }
-        break;
-      
-      case 3:
-        while (Serial.available() == 0 || Serial.read() != '4') {
-          Serial.println("Executing Smart Light IoT...");
-        }
-        break;
-
-      case 4:
-        Serial.println("Exiting program...");
-        exit(0);
-        break;
-
-      
-      default:
-        Serial.println("Invalid choice. Please try again.");
-        break;
-    }
-
-    Serial.println("");
+int printMenu() {
+  int choice;
+  bool getOk = false;
+  bool inputOk;
+  while (!getOk) {
     Serial.println("========================================");
+    Serial.println("🚀🚀     Welcome to MET_06 project   🚀🚀");
     Serial.println("           SELECT AN OPTION             ");
     Serial.println("========================================");
     Serial.println("1. Sensor Caidas IoT");
     Serial.println("2. Grifo IoT");
     Serial.println("3. Smart Light IoT");
     Serial.println("4. Salir");
+    
+    inputOk = false;
+    while (!inputOk) {
+      if (Serial.available()) {
+        inputOk = true;
+        choice = Serial.read() - '0'; // convert char to int
+        if (choice > 0 && choice <= 4) {
+          getOk = true;
+        }
+        else {
+          Serial.println("\nInvalid choice. Please try again.\n");
+        }
+      }
+    }
 
   }
+  return choice;
+}
+
+void setupByChoice(int opt) {
+  switch (opt) {
+    case MENU_SCAIDAS:
+      Serial.println("Initializating Sensor Caidas...");
+      setupSensorCaidas();
+      break;
+    case MENU_GRIFO:
+      Serial.println("Initializating Grifo IoT..."); 
+      setupGrifo();
+      break; 
+    case MENU_SLIGHT:
+      Serial.println("Initializating Smart Light IoT...");  
+      //setupSmartLight();
+      break;
+    case MENU_EXIT:
+      Serial.println("Exiting program...");  
+      exit(0);
+      break;
+  }
+  Serial.println("Executing...");
+}
+
+void executeByChoice(int opt) {
+  switch (opt) {
+    case MENU_SCAIDAS:
+      sensorCaidas();
+      break;
+    case MENU_GRIFO:
+      grifo();
+      break; 
+    case MENU_SLIGHT:
+      //smartLight();
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(SERIAL_CONNECTION_SPEED);
+  printConnection();
+  option = printMenu();
+  // Setup code. runs once
+  setupByChoice(option);
+
+}
+
+
+
+void loop() {
+  executeByChoice(option);
 }
