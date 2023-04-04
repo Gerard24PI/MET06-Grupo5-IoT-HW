3a83ed1019193fe4fc6b86d9fe7271bf016aa18c
Gerard PI <gerard.perez@students.salle.url.edu>
2023-04-04 14:56:33 +0200

    Adding Button feature to create objects in every project

---
+++ Button.cpp
@@ -0,0 +1,79 @@
+#include "Button.h"
+
+Button::Button() {
+}
+
+Button::Button(int pin) {
+  this->pin = pin;
+  pinMode(pin, INPUT);
+  this->button_state = 0;
+  this->rebound_time_ms = BUTTON_REBOUND_MS;
+  this->status = false;
+  this->button_timer = Timer();
+}
+
+void Button::setReboundTime(int time_ms) {
+  this->rebound_time_ms = time_ms;
+}
+
+bool Button::isPressed() {
+  if (status) {
+    status = false;
+    return true;
+  }
+  return false;
+}
+
+void Button::button_routine() {
+  switch(button_state) {
+    //Espera pulsació
+    case 0:
+      if (buttonConsult()) {
+        button_timer.setTimeReference();
+        button_state++;
+      }
+      break;
+    //Espera rebots pulsació
+    case 1:
+      if (esperaRebots()) {
+        if (buttonConsult()) {
+          status = true;
+          //update_mode(change_mode());
+          button_state++;
+        }
+        else {
+          button_state--;
+        }
+      }
+      break;
+    //Espera despulsació
+    case 2:
+      if (!buttonConsult()) {
+        button_timer.setTimeReference();
+        button_state++;
+      }
+      break;
+    //Espera rebots despulsació
+    case 3:
+      if (esperaRebots()) {
+        if (!buttonConsult()) {
+          button_state = 0;
+        }
+        else {
+          button_state--;
+        }
+      }
+  }
+}
+
+bool Button::buttonConsult() {
+  //Pull-Up interno
+  if (digitalRead(pin) == LOW) {
+    return true;
+  }
+  return false;
+}
+
+bool Button::esperaRebots() {
+  return button_timer.waitTime_ms(rebound_time_ms);
+}
---
+++ Button.h
@@ -0,0 +1,26 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <Arduino.h>
+#include "Timer.h"
+
+#define BUTTON_REBOUND_MS 16
+
+class Button {
+  public:
+    Button();
+    Button(int pin);
+    void setReboundTime(int time_ms);
+    bool isPressed();
+    void button_routine();
+  private:
+    int pin;
+    int button_state;
+    int rebound_time_ms;
+    bool status;
+    Timer button_timer;
+    bool buttonConsult();
+    bool esperaRebots();
+};
+
+#endif
