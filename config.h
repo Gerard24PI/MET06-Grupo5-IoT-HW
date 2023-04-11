d9a00e7b4dda48dfc314736ec3c5c2f6a4416b3a
Marc <marcnueno@gmail.com>
2023-04-11 10:35:12 +0200

    fixed pins

--- config.h
+++ config.h
@@ -26,9 +26,9 @@
   #define BOARD_NAME                  "ESP8266"
   #define PIN_PIR_SENSOR_BOTTOM       13           // D8 (Intermediate Board)
   #define PIN_PIR_SENSOR_TOP          15           // D7 (Final Board)
-  #define PIN_ALARM_LED_NOTIFICATION  12           // D6 (RED LED)
-  #define PIN_AUX_LED                 16           // D0 (ToDO: Connect)         
-  #define PIN_ANALOG_INPUT            2            // A0 (50K POT)
+  #define PIN_ALARM_LED_NOTIFICATION  16           // D6 (RED LED)
+  #define PIN_AUX_LED                 12            // SDD2          
+  #define PIN_ANALOG_INPUT            A0           // A0 (50K POT)
   #define PIN_ALARM_BUTTON            10           // SDD3 (RED BTN)
   #define PIN_MODE_BUTTON             5            // D1 (BTN 1)
   #define PIN_AUX_BUTTON              4            // D2 (BTN 2)
